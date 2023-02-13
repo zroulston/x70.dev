cf1efc7f61655dbc9022ab66befb997c2cf7dfb3
Zachary Roulston <zach@roulston.org>
2023-02-13 16:33:20 -0600

    Moving footer and disclaimer out to webassembly

--- js/scripts.js
+++ js/scripts.js
@@ -7,30 +7,55 @@ function waitForElement(elementPath, callBack){
         }, 50);
     }
 }
-waitForElement("#footer", function(){
-    var currentYear = new Date().getFullYear();
-    document.getElementById("footer").innerHTML = "[copyright &copy" + currentYear + " x70.dev - all rights reserved | <a id='disclaimerLink'>disclaimer</a> | <a id='gitHubLink'>source</a>]";
+
+var footer = "";
+
+//load our wasm file
+const go = new Go();
+WebAssembly.instantiateStreaming(fetch("assets/main.wasm"), go.importObject).then((result) => {
+  go.run(result.instance);
+  footer = footer + "<a id=wasmFooter></a>";
+  document.getElementById("footer").innerHTML = footer;
+});
+
+waitForElement("#wasmFooter", function(){
+    try {
+      document.getElementById("footer").innerHTML = getFooter();
+    } catch (error) {
+      console.log(error);
+      console.log("Error getting go footer. Defaulting to static footer.");
+      document.getElementById("footer").innerHTML = footer;
+    }
 
     var gitHubLink = document.getElementById("gitHubLink");
     gitHubLink.onclick = function() {
         window.open("https://github.com/zroulston/x70.dev", "_blank");
-    };
-    
-    var modal = document.getElementById("disclaimerModal");
-    var link = document.getElementById("disclaimerLink");
-    var span = document.getElementsByClassName("close")[0];
-       link.onclick = function() {
-      modal.style.display = "block";
-      return false;
     }
-    
-    span.onclick = function() {
+    //Fetch the disclaimer
+    try {
+      document.getElementById("disclaimerModal").innerHTML = getDisclaimer();
+    } catch (error) {
+      console.log(error);
+      console.log("Error getting go disclaimer.");
+    }    
+});
+
+waitForElement("#span", function(){
+  var modal = document.getElementById("disclaimerModal");
+  var link = document.getElementById("disclaimerLink");
+  var span = document.getElementsByClassName("close")[0];
+     link.onclick = function() {
+    modal.style.display = "block";
+    return false;
+  }
+  
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  window.onclick = function(event) {
+    if (event.target == modal) {
       modal.style.display = "none";
     }
-    
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-  }    
+}
 });
